b912e898510b947bac53ecd49e0f23652014f48e
TodaysJoey <zihyun.yi@gmail.com>
2023-12-04 22:18:25 +0900

    Feature: "Modal 기능별 분리"
    ModalItem은 공통으로 사용하기 위해 전체 레이아웃과 slot을 가진 컴포넌트로 분리
    
    Resolves: #22
    Ref: #18

--- src/components/ModalItem.vue
+++ src/components/ModalItem.vue
@@ -1,88 +1,21 @@
 <template>
-    <div class="modal">
-        <div class="container flex flex-col bg-white space-y-10 mx-auto my-20 w-4/6 h-2/6 px-7 py-5 rounded-lg">
-            <div class="flex space-x-2">
-                <span class="text-lg font-semibold">{{ props.title }}</span>
-            </div>
-            <div class="container max-w-md max-h-96 overflow-auto">
-                <div class="flex flex-col" v-if="props.type === 'email'">
-                    <input class="border p-2" type="email" id="email" placeholder="이메일을 입력해주세요." v-model="email" />
-                    <input class="border p-2" type="password" id="password" placeholder="패스워드를 입력해주세요."
-                        v-model="password" />
+    <teleport to="body">
+        <div class="modal">
+            <div class="container flex flex-col bg-white space-y-10 mx-auto my-20 w-4/6 h-2/6 px-7 py-5 rounded-lg">
+                <div class="flex space-x-2">
+                    <slot name="title"></slot>
                 </div>
-                <div class="flex flex-col" v-if="props.type === 'reset'">
-                    <input class="border p-2" type="email" id="resetEmail" placeholder="가입한 이메일 주소를 입력해주세요."
-                        v-model="resetEmail" />
-                    <span>입력한 이메일 주소로 비밀번호 재설정 메일이 전송됩니다.</span>
+                <div class="container max-w-md max-h-96 overflow-auto">
+                    <slot name="content"></slot>
                 </div>
-            </div>
-            <div class="flex space-x-2">
-                <button class="bg-blue-600 w-16 p-2 rounded font-bold text-base text-white"
-                    @click="clickConfirmBtn">확인</button>
-                <button class="border border-blue-600 w-16 p-2 rounded font-bold text-base text-blue-600"
-                    @click="clickCloseBtn">닫기</button>
+                <div class="flex space-x-2">
+                    <slot name="footer"></slot>
+                </div>
+
             </div>
         </div>
-
-    </div>
+    </teleport>
 </template>
-
-
-<script setup lang='ts'>
-import { ref } from 'vue'
-import { AuthInfo, type IUser, type IError } from '../assets/ts/auth'
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
-
-const props = defineProps({
-    type: String,
-    title: String
-})
-
-const email = ref('')
-const password = ref('')
-const resetEmail = ref('')
-
-const emit = defineEmits(['response', 'signUpCompl', 'resetCompl'])
-
-const clickCloseBtn = () => {
-    emit('response', false)
-}
-
-const clickConfirmBtn = async () => {
-    const auth = new AuthInfo();
-
-    if (props.type == 'email') {
-        let res: (IUser | IError) = await auth.signUp(email.value, password.value)
-
-        if (res.isSuccess === true) {
-            emit('signUpCompl', res)
-        } else {
-            toast.error(`가입에 실패했어요. (${(res as IError).errorCode}: ${(res as IError).errorMessage})`, {
-                position: toast.POSITION.BOTTOM_CENTER
-            })
-        }
-    }
-
-    if (props.type == 'reset') {
-        let res: (IUser | IError) = await auth.resetPassword(resetEmail.value)
-
-        if (res.isSuccess === true) {
-            emit('resetCompl', res)
-        } else {
-            toast.error(`비밀번호 재설정 메일 전송을 실패했어요. (${(res as IError).errorCode}: ${(res as IError).errorMessage})`, {
-                position: toast.POSITION.BOTTOM_CENTER
-            })
-        }
-
-    }
-
-
-}
-
-
-</script>
-
 
 <style scoped>
 .modal {
---
+++ src/components/modals/PasswordResetModal.vue
@@ -0,0 +1,60 @@
+
+<template>
+    <Modal ref="baseModal">
+        <template #title>
+            <span class="text-lg font-semibold">{{ props.title }}</span>
+        </template>
+        <template #content>
+            <div class="flex flex-col">
+                <input class="border p-2" type="email" id="resetEmail" placeholder="가입한 이메일 주소를 입력해주세요."
+                    v-model="resetEmail" />
+                <span>입력한 이메일 주소로 비밀번호 재설정 메일이 전송됩니다.</span>
+            </div>
+        </template>
+        <template #footer>
+            <button class="bg-blue-600 w-16 p-2 rounded font-bold text-base text-white" @click="confirm">확인</button>
+            <button class="border border-blue-600 w-16 p-2 rounded font-bold text-base text-blue-600"
+                @click="closeModal">닫기</button>
+        </template>
+    </Modal>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { AuthInfo, type IUser, type IError } from '../../assets/ts/auth'
+import Modal from '@/components/ModalItem.vue'
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+const props = defineProps({
+    type: String,
+    title: String
+})
+
+const resetEmail = ref('')
+
+const emit = defineEmits(['closeModal', 'resetCompl'])
+
+const closeModal = () => {
+    emit('closeModal', false)
+}
+
+const confirm = async () => {
+    const auth = new AuthInfo();
+
+    let res: (IUser | IError) = await auth.resetPassword(resetEmail.value)
+
+    if (res.isSuccess === true) {
+        emit('resetCompl', res)
+    } else {
+        toast.error(`비밀번호 재설정 메일 전송을 실패했어요. (${(res as IError).errorCode}: ${(res as IError).errorMessage})`, {
+            position: toast.POSITION.BOTTOM_CENTER
+        })
+    }
+
+}
+
+</script>
+
+
+<style scoped></style>
