7a43b90c05aa04aabfab6c8bb6c33aae11470dd7
TodaysJoey <zihyun.yi@gmail.com>
2023-12-09 18:34:50 +0900

    [#26] Feat: "DocumentEditor.vue 레이아웃 개발"
    에디터 페이지 레이아웃 초안 작성
    
    Related to: #26

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,10 +1,14 @@
 <script setup lang='ts'>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRouter } from "vue-router"
+import { useUserInfoStore } from '@/stores/user';
 
+const userStore = useUserInfoStore()
 
 const router = useRouter();
 const isLoginStatus = ref(false)
+
+
 const moveLoginPage = () => {
     router.push({
         path: '/login',
@@ -16,6 +20,11 @@ const goIndexPage = () => {
     });
 }
 
+watch(() => userStore.getLoginStatus, (cur, pre) => {
+    isLoginStatus.value = cur
+})
+
+
 </script>
 
 <template>
@@ -24,7 +33,7 @@ const goIndexPage = () => {
             <img class="cursor-pointer" src="../assets/images/logo.svg" alt="TexToDoc" @click="goIndexPage">
         </div>
         <div>
-            <button class="header-primary-btn" v-if="!isLoginStatus" @click="moveLoginPage">Sign In/Up</button>
+            <button class="header-primary-btn" v-if="!isLoginStatus" @click="moveLoginPage">Sign Up/In</button>
             <button class="header-primary-btn ml-1.5" v-if="isLoginStatus">로그아웃</button>
         </div>
     </div>
