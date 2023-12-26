1ed5649d7eeedb1d515e6bdb9e543e274bf44f7d
TodaysJoey <zihyun.yi@gmail.com>
2023-12-26 13:43:20 +0900

    [#49] Feat: "Editor.js 라이브러리 및 필요 플러그인 추가"
    Resolve #49

---
+++ src/components/editor/EditorItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="editorjs"></div>
+</template>
+
+<script setup lang='ts'>
+//import { ref } from 'vue'
+import EditorJS from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+import RawTool from '@editorjs/raw'
+// import List from "@editorjs/list"
+import Table from '@editorjs/table'
+import Marker from '@editorjs/marker'
+import NestedList from '@editorjs/nested-list'
+import Underline from '@editorjs/underline'
+import Warning from '@editorjs/warning';
+
+const editor = new EditorJS({
+    holder: 'editorjs',
+    tools: {
+        header: {
+            class: Header,
+            config: {
+                levels:[1,2,3,4],
+                defaultLevel: 1
+            }
+        },
+        raw: RawTool,
+        table: Table,
+        Marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M',
+        },
+        list: {
+            class: NestedList,
+            inlineToolbar: true,
+            config: {
+                defaultStyle: 'unordered'
+            }
+        },
+        underline: Underline,
+        warning: Warning,
+    }
+});
+
+
+</script>
+
+<style scoped>
+</style>
